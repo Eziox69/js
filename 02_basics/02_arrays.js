832b65e76a36d61d7a02f66bd8ebfbd4beb1a953
Eziox69 <106980196+Eziox69@users.noreply.github.com>
2024-02-08 10:41:01 +0000

    array2

---
+++ 02_basics/02_arrays.js
@@ -0,0 +1,4 @@
+ const marvel_heros=["ironman","spiderman","thor"]
+ const dc_heros=["superman","flash","batman"]
+ marvel_heros.push(dc_heros)//pushed array inside array
+ console.log(marvel_heros)

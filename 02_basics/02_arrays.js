dcee795181b8e9e619064122f9eb852dff6236d3
Eziox69 <106980196+Eziox69@users.noreply.github.com>
2024-02-08 16:30:16 +0000

    arrays2

--- 02_basics/02_arrays.js
+++ 02_basics/02_arrays.js
@@ -1,4 +1,7 @@
  const marvel_heros=["ironman","spiderman","thor"]
  const dc_heros=["superman","flash","batman"]
- marvel_heros.push(dc_heros)//pushed array inside array
- console.log(marvel_heros)
+ //marvel_heros.push(dc_heros)//pushed array inside array
+ //console.log(marvel_heros)//existing array pe hi push from push
+ //access karne k liye [3][0]-->give superman
+const all_heros=marvel_heros.concat(dc_heros)
+console.log(all_heros)//new array me

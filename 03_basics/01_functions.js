6927e1ee8d198f42c750ea2f40cdbde9b03ee5b5
Eziox69 <106980196+Eziox69@users.noreply.github.com>
2024-02-09 17:40:36 +0000

    func

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -29,14 +29,56 @@ const result=addTwonumbers(3,4)
 
 function loginUserMessage(username){//(username="sam")
     if(username===undefined){//or (!username)//undefined is false value
-          console.log("please enter a username")
+          //console.log("please enter a username")
            return//taaaki aage ka print na ho
         }
     return `${username} just logged in`
 }
 
 //console.log(loginUserMessage("hitesh"))//willl overwrite sam
-console.log(loginUserMessage())//undefined if we dont pass args
+//console.log(loginUserMessage())//undefined if we dont pass args
 
 
+////shopping cart 
+//isme args pta nhi hote
+//user add karta jaata hna samaaan
 
+function calculateCartPrice(...num1){
+   return num1
+}
+//console.log(calculateCartPrice(200,400,500))//kaafi values param ek to hum rest operator use karenge
+//rest operator is like spread ... use case pe differentiate kia jaat h
+
+function calculateCartPrice2(val1,val2, ...num1){
+    return num1
+ }
+ //onsole.log(calculateCartPrice2(200,400,500,1000,2000))// 200 val1,400 val2
+
+const user={
+    username:"hitesh",
+    price:199
+}
+
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price}`)
+}
+
+//handleObject(user)
+//OR
+//direct passkr skte h obj
+handleObject({
+    username: "sam",
+    price: 399
+
+})
+
+//passing array
+const myNewArray=[200,300,400,600]
+
+function returnSecondValue(getArray){
+    return getArray[1]
+
+}
+//console.log(returnSecondValue(myNewArray))
+console.log(returnSecondValue([200,300,400]))

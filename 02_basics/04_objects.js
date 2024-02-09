5893f01b3f801cfcd8df6603ec820daba3c4e2d4
Eziox69 <106980196+Eziox69@users.noreply.github.com>
2024-02-09 03:35:26 +0000

    obj

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -44,19 +44,39 @@ const user=[
     },
 ]
 user[1].email
-console.log(tinderUser);
-console.log(Object.keys(tinderUser))//sari keys dedo
+//console.log(tinderUser);
+//console.log(Object.keys(tinderUser))//sari keys dedo
 //output value ka data type h array
-console.log(Object.values(tinderUser))
+//console.log(Object.values(tinderUser))
 
-console.log(Object.entries(tinderUser))//array ke andar array bana deta hai first value key second value
+//console.log(Object.entries(tinderUser))//array ke andar array bana deta hai first value key second value
 
-console.log(tinderUser.hasOwnProperty('isLoggedIn'))//bata dega ye ki ye cheez obj me hai ki nhi
-
-
+//console.log(tinderUser.hasOwnProperty('isLoggedIn'))//bata dega ye ki ye cheez obj me hai ki nhi
 
 
 
+//destructuringgg
+
+const course={
+    coursename: "jshindi",
+    price:"999",
+    courseInstructor:"hitesh"
+
+}
+//course.courseInstructor//baar baar ye likhna padega 
+//instead
+const{courseInstructor}=course
+console.log(courseInstructor);
+const{courseInstructor:instructor}=course//aise rename bhi kar sakte h
+console.log(instructor);
+
+/*Api matlab apna kaam kisi aur pe daalne
+and use lena bs result
+apis humare paas json format me aati h
+wo obj jaisa dikhta hai
+kabhi kabhi arrays me bhi aaskati h
+json formatter me daalke hum samajh skate h apne json waale api ko
+json se obj banake fir usme work karna hota h*/
 
 
 

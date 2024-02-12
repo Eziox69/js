0a5191494c84aae9bea490b2a82ad1f9085dac49
Eziox69 <106980196+Eziox69@users.noreply.github.com>
2024-02-12 16:16:28 +0000

    cf

--- 04_controlflow/01.js
+++ 04_controlflow/01.js
@@ -5,31 +5,66 @@ if(true){
 //  strict -ve check !==
 
 
-const score= 200
-if(score> 100){
-    const power="fly"
-    //console.log(`User power: ${power}`)
-}
-//console.log(`User power: ${power}`)//will not work out of scope power
+// const score= 200
+// if(score> 100){
+//     const power="fly"
+//     //console.log(`User power: ${power}`)
+// }
+// //console.log(`User power: ${power}`)//will not work out of scope power
 
-const balance =1000
-if(balance<500){
-  //  console.log("less than 500");
-}else if(balance <900){
-  //  console.log("less tha 900")
-}else{
-   // console.log("less than")
-}
+// const balance =1000
+// if(balance<500){
+//   //  console.log("less than 500");
+// }else if(balance <900){
+//   //  console.log("less tha 900")
+// }else{
+//    // console.log("less than")
+// }
 
 const userLoggedIn=true
 const debitCard=true
 const loggedInFromGoogle=false
 const loggedInFromEmail=true
 
-if(userLoggedIn&&debitCard){//both should be true
-    console.log("allow to buy course")
-}
+// if(userLoggedIn&&debitCard){//both should be true
+//     console.log("allow to buy course")
+// }
 
-if(loggedInFromGoogle||loggedInFromEmail){
+// if(loggedInFromGoogle||loggedInFromEmail){
+//   console.log("user logged in")
     
+// }
+
+
+
+
+// switch (key) {
+//   case value:
+    
+//     break;
+
+//   default:
+//     break;
+
+const month=3
+
+switch (month) {
+  case 1:
+    console.log("January");
+    break;
+  case 2:
+    console.log("feb");
+    break;
+  case 3:
+    console.log("mar");
+    break;
+
+  default:
+    console.log("default")
+    break;
 }
+//har part me break hai kyuki  agar break nhi hota to bhi humara saara code execute horha hota even after finding the desired output
+//except default portion
+
+
+

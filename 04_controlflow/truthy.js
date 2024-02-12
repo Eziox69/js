04d14153bd0275f6e27f3ce6d6e6550bc3c56793
Eziox69 <106980196+Eziox69@users.noreply.github.com>
2024-02-12 16:38:43 +0000

    cf

---
+++ 04_controlflow/truthy.js
@@ -0,0 +1,38 @@
+//const userEmail="h@hotesh.ai"
+const userEmail=[]
+if(userEmail){
+    console.log("got user email");
+}else{
+    console.log("dont have user email")
+}
+//isme kisi cheez se comparison nhi hua still if is checking kyuki string ko true maana h
+// empty hoti to false mante
+
+/*
+falsy values
+false,0, -0, BigInt, " ", null,undefined, NaN
+
+truthy(everything except falsy)
+"0","false", " ",[],{},function(){}
+*/
+// if(userEmail.length===0){
+//   console.log("empty")
+// }
+
+// const emptyObj={}
+// if(Object.keys(emptyObj).length===0){//keys ka array aajata h banke
+//     console.log("empty")
+//   }
+
+//Nullish Coalescing Operator (??):null undefined
+
+//let val1;
+//val1 =5??10//op 5
+
+//val1=null??10                  op 10
+//val1=undefined??10             op 10
+//val1=null??10??20              op 10
+//console.log(val1)
+
+
+//Terniary Operator
